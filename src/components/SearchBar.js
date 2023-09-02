0f3f1a053394359f96197f64d2492158ff4ee95b
Ari Flowers <87172647+ari-flowers@users.noreply.github.com>
2023-09-02 14:35:29 -0700

    Handler set up

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,14 @@
-function SearchBar() {
-    return <div>SearchBar
+function SearchBar({ onSubmit }) {
+  const handleClick = () => {
+    onSubmit('cars')
+  }
+
+  return (
+    <div>
+      <input />
+      <button onClick={handleClick}>Click me</button>
     </div>
+    )
 }
 
 export default SearchBar;
